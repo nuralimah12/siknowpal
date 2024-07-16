1935b3b9a24ad4c60aee23aeda63bf6c2035539c
nualimah12 <alimahn123@gmail.com>
2024-07-16 07:57:14 +0700

    sign in revisi

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,17 +1,13 @@
 <div class="col-12">
   <div class="logo-wrapper d-flex gap-2">
-    <img src="{{asset('img/jti_logo.png')}}" class="footer-logo" />
-    <img src="{{asset('img/polinema_logo.png')}}" class="footer-logo" />
+    <img src="{{asset('img/logopal.png')}}" class="footer-logo" />
   </div>
   <div class="footer-content mt-3">
     <p class="m-0 fw-light footer-text departement">
-      Jurusan Teknologi Informasi Politeknik Negeri Malang
+      PT PAL INDONESIA
     </p>
     <p class="m-0 fw-light footer-text address">
-      JL. Soekarno-Hatta No. 9 Malang 65141
-    </p>
-    <p class="m-0 fw-light footer-text phone">
-      Telepon <span>+62 (0341) 404424 – 404425</span>
+      JL. Ujung, Ujung, Kec. Semampir, Surabaya, Jawa Timur
     </p>
   </div>
 </div>
